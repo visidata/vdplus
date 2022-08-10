af449dee66f0ab326a590dda0a95a4bbf8f0a3d0
s1291 <samirmath01@gmail.com>
2022-08-10 19:52:20 +0100

    Fix graphing
    Before the fix, `numericCols` was not defined. I have prefixed it with `graph` and added the `vd` argument since the function expects two arguments instead. I have tested this with visidata 2.9.1 and the matplotlib plotting is working.

--- graphing/seaborn.py
+++ graphing/seaborn.py
@@ -13,7 +13,7 @@ def plot_async(sheet):
     cat_array = []
 
     catcols = [c for c in sheet.xcols if not isNumeric(c)]
-    numcols = numericCols(sheet.xcols)
+    numcols = graph.numericCols(vd, sheet.xcols)
     for rownum, row in enumerate(Progress(sheet.sourceRows, 'plotting')):  # rows being plotted from source
         for ycol in sheet.ycols:
             try:
